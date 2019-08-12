ba195476b8005414bf9f99af2d2d843045454962
Iana <trifonova.yana.prof@gmail.com>
2019-08-12 20:50:32 +0300

    style for "how do we work" article

---
+++ js/swiper.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+};
