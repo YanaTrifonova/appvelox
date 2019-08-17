84cdce85580361b111eca8974be195a9b4de49c7
Iana <trifonova.yana.prof@gmail.com>
2019-08-17 16:45:15 +0300

    more ie fixes

--- js/moveSlider.js
+++ js/moveSlider.js
@@ -1,25 +1,7 @@
-/*function getStyleVal(elem, style) {
-    return Number(window.getComputedStyle(elem).getPropertyValue(style).slice(0, -2))
-}*/
 
 function moveSlide(e) {
     if (window.innerWidth > 1024) {
         let slider = document.getElementById("slider");
-        /*let parentElem = e.parentElement;
-        let children = [].slice.call(parentElem.children);
-        let paddingSlider = eparentElem.offsetLeft + getStyleVal(e, 'margin-left');
-        let isTheSame = false;
-        let ind = 0;
-        while (!isTheSame) {
-            let curChild = children[ind];
-            if (curChild === e) {
-                isTheSame = true;
-            } else {
-                paddingSlider += curChild.offsetLeft// + getStyleVal(curChild, 'margin-left') +
-                    //getStyleVal(curChild, 'margin-right');
-            }
-            ind += 1;
-        }*/
         slider.style.marginLeft = e.offsetLeft + "px";
         slider.style.width = e.offsetWidth + "px";
     }
