4e37a932c70384bf30fec5aa81f89ac90505ca21
Iana <trifonova.yana.prof@gmail.com>
2019-08-17 16:37:07 +0300

    simplified code for slider

--- js/moveSlider.js
+++ js/moveSlider.js
@@ -1,13 +1,13 @@
-function getStyleVal(elem, style) {
+/*function getStyleVal(elem, style) {
     return Number(window.getComputedStyle(elem).getPropertyValue(style).slice(0, -2))
-}
+}*/
 
 function moveSlide(e) {
     if (window.innerWidth > 1024) {
         let slider = document.getElementById("slider");
-        let parentElem = e.parentElement;
+        /*let parentElem = e.parentElement;
         let children = [].slice.call(parentElem.children);
-        let paddingSlider = parentElem.offsetLeft + getStyleVal(e, 'margin-left');
+        let paddingSlider = eparentElem.offsetLeft + getStyleVal(e, 'margin-left');
         let isTheSame = false;
         let ind = 0;
         while (!isTheSame) {
@@ -15,12 +15,12 @@ function moveSlide(e) {
             if (curChild === e) {
                 isTheSame = true;
             } else {
-                paddingSlider += curChild.offsetWidth + getStyleVal(curChild, 'margin-left') +
-                    getStyleVal(curChild, 'margin-right');
+                paddingSlider += curChild.offsetLeft// + getStyleVal(curChild, 'margin-left') +
+                    //getStyleVal(curChild, 'margin-right');
             }
             ind += 1;
-        }
-        slider.style.marginLeft = paddingSlider + "px";
+        }*/
+        slider.style.marginLeft = e.offsetLeft + "px";
         slider.style.width = e.offsetWidth + "px";
     }
 }
